3b407958c3e6cd1ee8995bc844861b6e3a61334c
Łukasz Bogucki <lukasz.bogucki137@gmail.com>
2023-02-22 20:36:35 +0100

    MainExtended Task

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String myCountry = "Poland";
+        int population = 37750000;
+        String countryCode = "PL";
+        boolean isEuropeanUnionMember = true;
+        double populationDensityPerSquareKilometer = 118.4;
+        String capitalCity = "Warsaw";
+        System.out.println(myCountry);
+        System.out.println(population);
+        System.out.println(countryCode);
+        System.out.println(isEuropeanUnionMember);
+        System.out.println(populationDensityPerSquareKilometer);
+        System.out.println(capitalCity);
+    }
+}
