5849a34f8bff04b434b6d8a42a1ceedb0bb27ea8
Łukasz Bogucki <lukasz.bogucki137@gmail.com>
2023-02-26 12:30:19 +0100

    Simple Arrays: Task 1

---
+++ kodilla-intro/src/main/java/SimpleArray.java
@@ -0,0 +1,12 @@
+public class SimpleArray {
+    public static void main(String[] args) {
+        String[] animals = new String[7];
+        animals[0] = "Lion";
+        animals[1] = "Elephant";
+        animals[2] = "Fox";
+        animals[3] = "Rabbit";
+        animals[4] = "Wolf";
+        animals[5] = "Bear";
+        animals[6] = "Giraffe";
+    }
+}
