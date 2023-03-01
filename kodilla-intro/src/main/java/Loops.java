36cbf7e60eacb0499db5348dee3f3a7940a44ad7
Łukasz Bogucki <lukasz.bogucki137@gmail.com>
2023-03-01 17:48:11 +0100

    Loops: Task 1

--- kodilla-intro/src/main/java/Loops.java
+++ kodilla-intro/src/main/java/Loops.java
@@ -1,7 +1,9 @@
 public class Loops {
     public static void main(String[] args) {
-        for (int i = 0; i <= 10; i++) {
-            System.out.println(i);
+        String[] names = new String[] {"Zygfryd", "Gwidon", "Florentyna"};
+        int numberOfElements = names.length;
+        for (int i = 0; i < numberOfElements; i++) {
+            System.out.println(names[i]);
         }
     }
 }
