566c725b049578c7000711ddf616efc37ade45de
Łukasz Bogucki <lukasz.bogucki137@gmail.com>
2023-03-10 17:28:24 +0100

    Objects in Java: tasks

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,6 +1,15 @@
 public class FirstClass {
     public static void main(String[] args) {
-        String myWelcome = "Hello from FirstClass!";
-        System.out.println(myWelcome);
+        Notebook notebook = new Notebook(600, 2500);
+        System.out.println(notebook.weight + " " + notebook.price);
+        notebook.checkPrice();
+
+        Notebook heavyNotebook = new Notebook(2000, 4500);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        heavyNotebook.checkPrice();
+
+        Notebook oldNotebook = new Notebook(1200, 700);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        oldNotebook.checkPrice();
     }
 }
