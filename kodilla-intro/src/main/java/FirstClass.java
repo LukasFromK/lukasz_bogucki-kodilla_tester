ec33151b592f45cf28a304f3cf3517df18c4c863
Łukasz Bogucki <lukasz.bogucki137@gmail.com>
2023-03-11 13:51:13 +0100

    Module 2. Task: Conditional instructions

--- kodilla-intro/src/main/java/FirstClass.java
+++ kodilla-intro/src/main/java/FirstClass.java
@@ -1,15 +1,51 @@
 public class FirstClass {
     public static void main(String[] args) {
-        Notebook notebook = new Notebook(600, 2500);
-        System.out.println(notebook.weight + " " + notebook.price);
+        Notebook notebook = new Notebook(600, 2500, 2021);
+        System.out.println(notebook.weight + " " + notebook.price + " " + notebook.year);
         notebook.checkPrice();
+        notebook.checkWeight();
+        notebook.checkValueForMoney();
 
-        Notebook heavyNotebook = new Notebook(2000, 4500);
-        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price);
+        Notebook heavyNotebook = new Notebook(2000, 4500, 2017);
+        System.out.println(heavyNotebook.weight + " " + heavyNotebook.price + " " + heavyNotebook.year);
         heavyNotebook.checkPrice();
+        heavyNotebook.checkWeight();
+        heavyNotebook.checkValueForMoney();
 
-        Notebook oldNotebook = new Notebook(1200, 700);
-        System.out.println(oldNotebook.weight + " " + oldNotebook.price);
+        Notebook oldNotebook = new Notebook(1200, 700, 2010);
+        System.out.println(oldNotebook.weight + " " + oldNotebook.price + " " + oldNotebook.year);
         oldNotebook.checkPrice();
+        oldNotebook.checkWeight();
+        oldNotebook.checkValueForMoney();
+
+        Notebook quiteNewNotebook = new Notebook(800, 5000, 2018);
+        System.out.println(quiteNewNotebook.weight + " " + quiteNewNotebook.price + " " + quiteNewNotebook.year);
+        quiteNewNotebook.checkPrice();
+        quiteNewNotebook.checkWeight();
+        quiteNewNotebook.checkValueForMoney();
+
+        Notebook quiteOldNotebook = new Notebook(2000, 150, 2000);
+        System.out.println(quiteOldNotebook.weight + " " + quiteOldNotebook.price + " " + quiteOldNotebook.year);
+        quiteOldNotebook.checkPrice();
+        quiteOldNotebook.checkWeight();
+        quiteOldNotebook.checkValueForMoney();
+
+        Notebook extremelyOldNotebook = new Notebook(2500, 1000, 1995);
+        System.out.println(extremelyOldNotebook.weight + " " + extremelyOldNotebook.price + " " + extremelyOldNotebook.year);
+        extremelyOldNotebook.checkPrice();
+        extremelyOldNotebook.checkWeight();
+        extremelyOldNotebook.checkValueForMoney();
+
+        Notebook smallNotebook = new Notebook(300, 500, 2020);
+        System.out.println(smallNotebook.weight + " " + smallNotebook.price + " " + smallNotebook.year);
+        smallNotebook.checkPrice();
+        smallNotebook.checkWeight();
+        smallNotebook.checkValueForMoney();
+
+        Notebook weirdNotebook = new Notebook (700, 350, 2014);
+        System.out.println( weirdNotebook.weight + " " + weirdNotebook.price + " " + weirdNotebook.year);
+        weirdNotebook.checkPrice();
+        weirdNotebook.checkWeight();
+        weirdNotebook.checkValueForMoney();
     }
 }
--- kodilla-intro/src/main/java/Notebook.java
+++ kodilla-intro/src/main/java/Notebook.java
@@ -1,16 +1,49 @@
 public class Notebook {
         int weight;
         int price;
-        public Notebook(int weight, int price) {
+        int year;
+
+        public Notebook(int weight, int price, int year) {
                 this.weight = weight;
                 this.price = price;
+                this.year = year;
         }
 
         public void checkPrice() {
-                if (this.price < 900) {
-                        System.out.println("This notebook is cheap!");
+                if (this.price < 600) {
+                        System.out.println("This notebook is very cheap.");
+                } else if (this.price >= 600 && this.price < 1001) {
+                        System.out.println("This price is good.");
                 } else {
-                        System.out.println("This notebook is quite expensive!");
+                        System.out.println("This notebook is expensive.");
+                }
+        }
+
+        public void checkWeight() {
+                if (this.weight <= 500) {
+                        System.out.println("This device is lightweight.");
+                } else if (this.weight > 500 && this.weight <=1500) {
+                        System.out.println("This device is not too heavy.");
+                } else {
+                        System.out.println("This device is very heavy.");
+                }
+        }
+
+        public void checkValueForMoney() {
+                if (this.year >= 2018 && this.price <= 600) {
+                        System.out.println("This device is quite new and it is very good value for money!");
+                } else if (this.year >= 2018 && (this.price > 600 && this.price <= 3000)) {
+                        System.out.println("This device is quite new and it is quite good value for money.");
+                } else if ((this.year < 2018 && this.year >= 2012) && this.price <= 400) {
+                        System.out.println("This device is very good value for money!");
+                } else if ((this.year < 2018 && this.year >= 2012) && (this.price > 400 && this.price <= 2000)) {
+                        System.out.println("This device is quite good value for money.");
+                } else if (this.year < 2012 && this.price <= 200) {
+                        System.out.println("This device is old, although it is very good value for money!");
+                } else if (this.year < 2012 && (this.price > 200 && this.price <= 700)) {
+                        System.out.println("This device is old, although it is quite good value for money.");
+                } else {
+                        System.out.println("This device is bad value for money. Don't buy it!");
                 }
         }
 }
