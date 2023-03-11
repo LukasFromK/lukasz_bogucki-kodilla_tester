6c163323ab8183333b5e81218c189ffdb007ad5c
Łukasz Bogucki <lukasz.bogucki137@gmail.com>
2023-03-11 23:22:03 +0100

    Module 2. Task 4

---
+++ kodilla-intro/src/main/java/Grades.java
@@ -0,0 +1,31 @@
+public class Grades {
+    private int[] grades;
+    private int size;
+
+    public Grades() {
+        this.grades = new int[10];
+        this.size = 0;
+    }
+
+    public void add(int value) {
+        if (this.size == 10) {
+            return;
+        }
+        this.grades[this.size] = value;
+        this.size++;
+    }
+
+    public int lastGrade() {
+        int i = size - 1;
+        return grades[i];
+    }
+
+    public double average(int[] grades) {
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            sum = sum + grades[i];
+        }
+        double result = sum / size;
+        return result;
+    }
+}
